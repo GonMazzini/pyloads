3263f119285ba0882074bf7fc5bca9c108dd33f9
GonMazzini <gmazzini@itba.edu.ar>
2021-01-02 01:52:51 -0300

    add def flow_angle

---
+++ pyloads/__init__.py
@@ -0,0 +1,4 @@
+from static_loads import Rotor
+
+rot = Rotor(rho=1.225)
+print(rot.lift_drag_coeff(t=5.38000000E+00))
--- pyloads/static_loads.py
+++ pyloads/static_loads.py
@@ -1,8 +1,8 @@
 """Standard library imports"""
-import os
 import numpy as np  # 1.19.4
 import pandas as pd  # 1.2.0
-import scipy as sp  # 1.6.0
+import scipy as sp  #
+from scipy.interpolate import interp1d
 
 print(f'numpy vers {np.__version__} , \t pandas vers {pd.__version__} , \t scipy vers {sp.__version__}')
 
@@ -29,35 +29,85 @@ class Rotor():
 
     ffa_dict = dict(zip(np.arange(6), [ffa_241, ffa_301, ffa_360, ffa_480, ffa_600, cylinder]))
 
-    """Load the balde data: twist, chord and thick/chord relation for corresponding
-    radial position."""
-
     blade_data = pd.read_csv('bladedat.txt', sep='\t', names=['r', 'twist', 'c', 't/c'])
 
     # class constructor
 
     def __init__(self, rho=1.225):
-        self.rho = rho # [kg/m3]
+        self.rho = rho  # [kg/m3]
 
     @classmethod
     def get_blade_data(cls):
+        """Display the blade data: twist, chord and thick/chord relation for corresponding
+        radial position."""
         return cls.blade_data
 
-    def load_calc(self, wind_speed):
+    def lift_drag_coeff(self, t, alpha):
+        # TODO:
+        #   raise exceptions for thick and alpha
+        """Interpolation for drag and lift coefficients.
+
+         Returns: (Cl, Cd)
+         :type t: float"""
+        cdthick, clthick = np.zeros(6), np.zeros(6)
+
+        for k in range(6):
+            f1cl = interp1d(Rotor.ffa_dict[k].iloc[:, 0], Rotor.ffa_dict[k].iloc[:, 1])
+            f1cd = interp1d(Rotor.ffa_dict[k].iloc[:, 0], Rotor.ffa_dict[k].iloc[:, 2])
+            clthick[k] = f1cl(alpha * 180 / np.pi)  # must convert into degrees
+            cdthick[k] = f1cd(alpha * 180 / np.pi)
+
+        thick_prof = np.array([24.1, 30.1, 36., 48., 60., 100.])
+        f2cl = interp1d(thick_prof, clthick)
+        f2cd = interp1d(thick_prof, cdthick)
+        Cl = f2cl(t)
+        Cd = f2cd(t)
+        print(f'Cd= {Cd} \t Cl= {Cl}')
+        return Cl, Cd
+
+    def load_calc(self, wind_speed, pitch, rpm):
+        v0 = wind_speed
+
+        try:
+            v0 + 2
+        except TypeError:
+            print('The wind speed should be a number (int or float)')
+
+        assert (v0 > 0), 'The wind speed should be positive'
+
         print(len(Rotor.blade_data))
         # assert isinstance(wind_speed, object)
-        v0 = wind_speed
+
         print(self.rho)
         for row in range(len(Rotor.blade_data)):
-            #a, a_prime, k, i, conv, ac, diff = 0, 0, 0, 0, 1, 1 / 3, 0.0001
-            r, c, t, twist = Rotor.blade_data['r'][row], Rotor.blade_data['c'][row], Rotor.blade_data['t/c'][row], Rotor.blade_data['twist'][row]
-            print(r,c,t,twist)
+            # FIXME: complete loads calculation code.
+            a, a_prime, k, i, conv, ac, diff = 0, 0, 0, 0, 1, 1 / 3, 0.0001
+            r, c, t, twist = Rotor.blade_data['r'][row], Rotor.blade_data['c'][row], Rotor.blade_data['t/c'][row], \
+                             Rotor.blade_data['twist'][row]
+            while k == 0:
+                i += 1
+                flow_angle = np.arctan(((1 - a) * v0) / ((1 + a_prime) * w * r))
+                alpha = flow_angle - (twist * np.pi / 180 + pitch * np.pi / 180)  # alpha in radians
 
-        return r
+                Cn = Cl * np.cos(flow_angle) + Cd * np.sin(flow_angle)
+                Ct = Cl * np.sin(flow_angle) - Cd * np.cos(flow_angle)
+
+                v_rel = ((1 - a) * v0) / np.sin(flow_angle)
+
+                pN_load = 0.5 * rho * v_rel ** 2 * c * Cn
+                pT_load = 0.5 * rho * v_rel ** 2 * c * Ct
+
+                F = (2 / np.pi) * np.arccos(np.exp(-(B / 2) * ((R - r) / (r * np.sin(abs(flow_angle))))))
+
+                sigma = c * B / (2 * np.pi * r)
+
+        return a
 
     def flow_angle(self):
+        # TODO: define flow angle method.
         pass
 
 
-rotor = Rotor()
-rotor.load_calc(wind_speed=9)
+if __name__ == "__main__":
+    rotor = Rotor()
+    rotor.load_calc(wind_speed=9)
