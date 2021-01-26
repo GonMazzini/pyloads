bf3acfcdf6e8a6eb1eca7d370e8a7dda167c0c66
GonMazzini <gmazzini@itba.edu.ar>
2021-01-26 05:46:08 -0300

    add aero module

--- pyloads/static_loads.py
+++ pyloads/static_loads.py
@@ -8,6 +8,7 @@ from scipy.optimize import fsolve
 
 """Local modules"""
 from pyloads.blade_data import BladeFeatures
+from pyloads.aerodynamic_profiles import AeroProfiles
 
 print(f'numpy version {np.__version__} , \t pandas vers {pd.__version__} , \t scipy vers {sp.__version__}')
 
@@ -25,42 +26,42 @@ class Rotor:
     radio = 89.17  # [m]
     rho = 1.225  # [km/m3]
 
-    """Load the aerodynamics profiles."""
-    # TODO:
-    #   This should be in a child class from Rotor called DTU-10MW
-    ffa_241 = pd.read_csv('FFA-W3-241.txt', sep='\t', names=['alpha', 'Cl', 'Cd', 'Cm'])
-    ffa_301 = pd.read_csv('FFA-W3-301.txt', sep='\t', names=['alpha', 'Cl', 'Cd', 'Cm'])
-    ffa_360 = pd.read_csv('FFA-W3-360.txt', sep='\t', names=['alpha', 'Cl', 'Cd', 'Cm'])
-    ffa_480 = pd.read_csv('FFA-W3-480.txt', sep='\t', names=['alpha', 'Cl', 'Cd', 'Cm'])
-    ffa_600 = pd.read_csv('FFA-W3-600.txt', sep='\t', names=['alpha', 'Cl', 'Cd', 'Cm'])
-    cylinder = pd.read_csv('cylinder.txt', sep='\t', names=['alpha', 'Cl', 'Cd', 'Cm'])
 
-    ffa_dict = dict(zip(np.arange(6), [ffa_241, ffa_301, ffa_360, ffa_480, ffa_600, cylinder]))
 
     # blade_data = pd.read_csv('bladedat.txt', sep='\t', names=['r', 'twist', 'c', 't/c'])
 
     # class constructor
 
-    def __init__(self, radio=np.array([2.8, 11.0027383, 16.871021, 22.9641225, 32.3076383,
-                                       41.5730197, 50.4110617, 58.5344352, 65.7500952, 71.9674921,
-                                       77.1859743, 78.7133469, 80.1402166, 82.7084849, 84.9251565,
-                                       86.8264859, 88.4486629, 89.166])
-                 , twist=np.array([14.5, 14.4320088, 12.5459743, 8.89245229, 6.38219779,
-                                   4.67357583, 2.88655238, 1.20718981, -0.13037962, -1.11392478,
-                                   -1.86195876, -2.0776268, -2.2800794, -2.64408409, -2.94507068,
-                                   -3.17732896, -3.35752648, -3.428])
-                 , cord=np.array([5.38, 5.45248001, 5.86600432, 6.18451124, 6.01996243,
-                                  5.42189119, 4.69821952, 3.99867965, 3.39809449, 2.91304006,
-                                  2.53594729, 2.4307714, 2.33143176, 2.12906893, 1.9033225,
-                                  1.62601186, 1.18359898, 0.6])
-                 , t_c=np.array([100., 86.0491476, 61.0969583, 43.0356446, 32.4150527,
-                                 27.8103477, 25.317976, 24.2644205, 24.1006815, 24.1,
-                                 24.1000016, 24.1000006, 24.1, 24.1, 24.1,
-                                 24.1, 24.1, 24.1])):
-        self.radio = radio
-        self.twist = twist
-        self.cord = cord
-        self.t_c = t_c
+    def __init__(self, radio=None, twist=None, cord=None, t_c=None, profiles='Default'):
+        bld = BladeFeatures()
+        # Take the parameters from BladeFeatures corresponding to DTU 10MW
+        if radio is None:
+            self.radio = bld.radio
+
+        if twist is None:
+            self.twist = bld.twist
+
+        if cord is None:
+            self.cord = bld.cord
+
+        if t_c is None:
+            self.t_c = bld.t_c
+
+        """Load the aerodynamics profiles."""
+        # TODO:
+        #   This should be in a child class from Rotor called DTU-10MW
+        if profiles == 'Default':
+            aero_prof=AeroProfiles()
+
+            ffa_241 = aero_prof.ffa_241
+            ffa_301 = aero_prof.ffa_301
+            ffa_360 = aero_prof.ffa_360
+            ffa_480 = aero_prof.ffa_480
+            ffa_600 = aero_prof.ffa_600
+            cylinder = aero_prof.cylinder
+
+            self.ffa_dict = dict(zip(np.arange(6), [ffa_241, ffa_301, ffa_360, ffa_480, ffa_600, cylinder]))
+
         # TODO:
         #   check that the len of the features is the same
         #   check that the data has valid ranges..
@@ -102,8 +103,8 @@ class Rotor:
         cdthick, clthick = np.zeros(6), np.zeros(6)
 
         for k in range(6):
-            f1cl = interp1d(Rotor.ffa_dict[k].iloc[:, 0], Rotor.ffa_dict[k].iloc[:, 1])
-            f1cd = interp1d(Rotor.ffa_dict[k].iloc[:, 0], Rotor.ffa_dict[k].iloc[:, 2])
+            f1cl = interp1d(self.ffa_dict[k].iloc[:, 0], self.ffa_dict[k].iloc[:, 1])
+            f1cd = interp1d(self.ffa_dict[k].iloc[:, 0], self.ffa_dict[k].iloc[:, 2])
             clthick[k] = f1cl(alpha * 180 / np.pi)  # must convert into degrees
             cdthick[k] = f1cd(alpha * 180 / np.pi)
 
