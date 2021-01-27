9348eb8808a9571480bd4438c07cec3a274bfb0e
GonMazzini <gmazzini@itba.edu.ar>
2021-01-27 07:31:24 -0300

    readme update

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools
 setup(
     name='pyloads-wind-turbine',  # How you named your package folder (MyLib)
     packages=setuptools.find_packages(),  # Chose the same as "name"
-    version='1.0.1',  # Start with a small number and increase it with every change you make
+    version='1.0.2',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Loads and deflection for wind turbine blade.',  # Give a short description about your library
     author='Gonzalo Mazzini',  # Type in your name
     author_email='gmazzini@itba.edu.ar',  # Type in your E-Mail
-    url='https://github.com/GonMazzini/pyloads/tree/v1.0.1',  # Provide either the link to your github or to your website
-    download_url='https://github.com/GonMazzini/pyloads/archive/v1.0.1.tar.gz',  # I explain this later on
+    url='https://github.com/GonMazzini/pyloads',  # Provide either the link to your github or to your website
+    download_url='https://github.com/GonMazzini/pyloads/archive/v.0.0.1.tar.gz',  # I explain this later on
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
